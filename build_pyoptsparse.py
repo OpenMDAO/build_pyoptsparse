68d787f4f421a6fbcb3ac4ee15c7aace69b38bcd
Rob Falck <rfalck@nasa.gov>
2025-05-06 15:44:20 -0400

    cleanup

--- build_pyoptsparse.py
+++ build_pyoptsparse.py
@@ -1202,23 +1202,17 @@ If it does, set up Intel OneAPI {yellow('before')} activating your conda env.
 
 def select_intel_compilers():
     """ Set environment variables to use Intel compilers. """
-    if 'CC' not in os.environ:
-        os.environ['CC'] = 'icc'
-    if 'CXX' not in os.environ:
-        os.environ['CXX'] = 'icpc'
-    if 'FC' not in os.environ:
-        os.environ['FC'] = 'ifort'
+    os.environ.get('CC', 'icc')
+    os.environ.get('CXX', 'icpc')
+    os.environ.get('FC', 'ifort')
     sys_info['gcc_major_ver'] = -1
     sys_info['gcc_is_apple_clang'] = False
 
 def select_gnu_compilers():
     """ Set environment variables to use GNU compilers. """
-    if 'CC' not in os.environ:
-        os.environ['CC'] = 'gcc'
-    if 'CXX' not in os.environ:
-        os.environ['CXX'] = 'g++'
-    if 'FC' not in os.environ:
-        os.environ['FC'] = 'gfortran'
+    os.environ.get('CC', 'gcc')
+    os.environ.get('CXX', 'g++')
+    os.environ.get('FC', 'gfortran')
     gcc_ver = subprocess.run(['gcc', '-dumpversion'], capture_output=True)
     sys_info['gcc_major_ver'] = int(gcc_ver.stdout.decode('UTF-8').split('.')[0])
     gcc_version = subprocess.run(['gcc', '--version'], capture_output=True)
